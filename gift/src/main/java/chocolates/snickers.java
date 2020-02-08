d99894ebb37ce3c9564f2a5c1a25da51a9668cba
anudeep4c4 <60147856+anudeep4c4@users.noreply.github.com>
2020-02-08 23:13:05 +0530

    Update snickers.java

--- gift/src/main/java/chocolates/snickers.java
+++ gift/src/main/java/chocolates/snickers.java
@@ -1,10 +1,10 @@
 package chocolates;
 
 public class snickers extends chocolate{
-	public int weight()
+	public int weight(int x)
 	{
 	int weight=50;
-	return weight;
+	return weight*x;
 	}
 	public int price(int x)
 	{
